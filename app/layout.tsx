32381ab6371cf3a263c8d2534eadf51841e238ed
paardhavlaveti <139532356+paardhavlaveti@users.noreply.github.com>
2025-03-13 19:36:54 +0530

    ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     return (
         <html lang="en">
         
-        <body>
+        <body className="bg-black">
             
             <NavbarDemo />
             
